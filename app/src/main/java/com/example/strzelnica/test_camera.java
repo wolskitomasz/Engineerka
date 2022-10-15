a786391a63c455ee91aa08293a9409387cd28355
QbA0410 <jakub.blazej3@gmail.com>
2022-10-15 22:32:34 +0200

    Obrócono poprawnie ekran + dodano kontrolę jasności.

--- app/src/main/java/com/example/strzelnica/test_camera.java
+++ app/src/main/java/com/example/strzelnica/test_camera.java
@@ -2,14 +2,19 @@ package com.example.strzelnica;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.media.MediaMetadataRetriever;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.SurfaceView;
+
+import com.google.android.material.slider.Slider;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.JavaCameraView;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
@@ -18,10 +23,12 @@ import org.opencv.imgproc.Imgproc;
 
 public class test_camera extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
+    Slider slider;
     JavaCameraView javaCameraView;
-    Mat mRGBA, mRGBAT;
+    Mat mRGBAT;
     Mat mat1, mat2;
     Scalar scalarLow, scalarHigh;
+    Mat src;
 
 
     BaseLoaderCallback baseLoaderCallback = new BaseLoaderCallback(test_camera.this) {
@@ -53,7 +60,7 @@ public class test_camera extends AppCompatActivity implements CameraBridgeViewBa
         javaCameraView = findViewById(R.id.javaCameraView);
 
         javaCameraView.setCameraIndex(0);
-        scalarLow = new Scalar(0,150,50);
+        scalarLow = new Scalar(0,70,50);
         scalarHigh = new Scalar(10,255,255);
 
 //        javaCameraView.setVisibility(SurfaceView.VISIBLE);
@@ -64,15 +71,15 @@ public class test_camera extends AppCompatActivity implements CameraBridgeViewBa
 
     @Override
     public void onCameraViewStarted(int width, int height) {
-//        mRGBA = new Mat(height, width, CvType.CV_8UC4);
+        src = new Mat(height, width, CvType.CV_8UC4);
 
-        mat1 = new Mat(width, height, CvType.CV_16UC4);
-        mat2 = new Mat(width, height, CvType.CV_16UC4);
+        mat1 = new Mat(height, width, CvType.CV_8UC4);
+        mat2 = new Mat(height, width, CvType.CV_8UC4);
     }
 
     @Override
     public void onCameraViewStopped() {
-        mRGBA.release();
+        src.release();
     }
 
     @Override
@@ -81,18 +88,28 @@ public class test_camera extends AppCompatActivity implements CameraBridgeViewBa
 //        mRGBAT = mRGBA.t();
 //        Core.flip(mRGBA.t(), mRGBAT, 1);
 //        Imgproc.resize(mRGBAT, mRGBAT, mRGBA.size());
+//
 //        return mRGBAT;
 
 
-        Mat src = inputFrame.rgba();
+        src = inputFrame.rgba();
+        mRGBAT = src.t();
 
+        Core.flip(src.t(), mRGBAT, 1);
+        Imgproc.resize(mRGBAT, mRGBAT, src.size());
         Imgproc.cvtColor(inputFrame.rgba(), mat1, Imgproc.COLOR_BGR2HSV);
         Core.inRange(mat1,scalarLow,scalarHigh,mat2);
         Core.MinMaxLocResult mmG = Core.minMaxLoc(mat2);
 
-        Imgproc.circle(src, mmG.maxLoc, 30, new Scalar(0,255,0), 5, Imgproc.LINE_AA);
+        Imgproc.circle(mRGBAT, mmG.maxLoc, 30, new Scalar(0,255,0), 5, Imgproc.LINE_AA);
 
-        return src;
+        slider = findViewById(R.id.slider);
+        int brightness = (int) slider.getValue();
+
+        mRGBAT.convertTo(mRGBAT,-1,1,brightness);
+
+
+        return mRGBAT;
 
     }
 
