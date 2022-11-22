0873d08d53f55dc63452a89e765c38efe3e20b58
wolskitomasz <tw.tomaszwolski@gmail.com>
2022-11-22 11:07:18 +0100

    Zmiana jezyka na formatce User

--- app/src/main/java/com/example/strzelnica/ActivityUser.java
+++ app/src/main/java/com/example/strzelnica/ActivityUser.java
@@ -6,7 +6,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class ActivityUser extends AppCompatActivity {
 
@@ -15,8 +21,42 @@ public class ActivityUser extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
 
+        //Sprawdzenie języka
+        StringBuffer datax2 = new StringBuffer("");
+        try {
+            FileInputStream fIn = openFileInput ( "jezyk" ) ;
+            InputStreamReader isr = new InputStreamReader ( fIn ) ;
+            BufferedReader buffreader = new BufferedReader ( isr ) ;
+
+            String readString = buffreader.readLine ( ) ;
+            while ( readString != null ) {
+                datax2.append(readString);
+                readString = buffreader.readLine ( ) ;
+            }
+
+            isr.close ( ) ;
+        } catch ( IOException ioe ) {
+            ioe.printStackTrace ( ) ;
+        }
+
         Button buttonGuest = findViewById(R.id.buttonGuest);
         Button buttonLogin = findViewById(R.id.buttonLogin);
+        TextView textView = findViewById(R.id.textView);
+
+        //zmiana jezyka
+        if(datax2.toString().equals("Polski"))
+        {
+            textView.setText("UŻYTKOWNIK");
+            buttonGuest.setText("GOŚĆ");
+            buttonLogin.setText("ZALOGUJ");
+
+        }
+        else
+        {
+            textView.setText("USER");
+            buttonGuest.setText("GUEST");
+            buttonLogin.setText("LOG IN");
+        }
 
         buttonGuest.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -28,7 +68,14 @@ public class ActivityUser extends AppCompatActivity {
         buttonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(),"DO ZOBACZENIA WKRÓTCE", Toast.LENGTH_LONG).show();
+                if(datax2.toString().equals("Polski"))
+                {
+                    Toast.makeText(getApplicationContext(), "DO ZOBACZENIA WKRÓTCE", Toast.LENGTH_LONG).show();
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(), "SEE YOU SOON", Toast.LENGTH_LONG).show();
+                }
             }
         });
 
